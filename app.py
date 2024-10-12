1896cd2715e9502041e52497a53651ffc6714bb3
ronilpatil <ronilpatil2000@gmail.com>
2024-10-12 10:07:51 +0530

    session-2 [dynamic URL, URL redirection & building]

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import time
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,27 +10,46 @@ def home():
     return "<h1>Welcome to home page</h1>"
 
 
-# here the name parameter by default taking string
+# --------- dynamic URL concept ---------
+# here the name parameter is dynamic parameter
 @app.route("/welcome/<name>")  # path parameter
 def welcome(name):
     # whatever we will pass in endpoint, it'll be reflected here
     return f"<h1>Hi {name.title()}, welcome to the page!</h1>"
 
 
-# lets pass num and chage its data type
-@app.route("/addition/<int:num>")  # path parameter
-def addition(num):
-    return f"<h1>Num is {num}, Addition is {num+10}</h1>"
+# ------- URL Redirection --------
+# why URL redirection?
+# if page is deleted, user need to redirected to another page
+# if maintainance of page is going on
+# if website gets renamed, so user should be redirectd to appropriate URL
+@app.route("/score/<name>/<int:score>")
+def score(name, score):
+    if score < 30:
+        time.sleep(1)
+        # redirect to failed page
+        # here we can pass full url while redirecting like https://** but to reduce manual efforts use url_for, it'll do this for us
+        return redirect(
+            # as passed is expecting name parameter so pass parameter in url_for function itself
+            url_for("failed", name=name)
+        )  # pass function name, not endpoint
+    else:
+        time.sleep(1)
+        # redirect to passed page
+        # as passed is expecting name parameter so pass parameter in url_for function itself
+        return redirect(url_for("passed", name=name))
 
 
-@app.route("/addition_two/<int:num1>/<int:num2>")  # path parameter
-def addition_two(num1, num2):
-    return f"<h1>{num1} + {num2} = {num1 + num2}</h1>"
+# redrirected to passed
+@app.route("/pass/<name>")
+def passed(name):
+    return f"<h1>Congrats {name.title()}, you've cleared the xm.</h1>"
 
 
-@app.route("/about")
-def about():
-    return "<h1>This is about page</h1>"
+# redirected to failed
+@app.route("/failed/<name>")
+def failed(name):
+    return f"<h1>Sorry {name.title()}, better luck next time.</h1>"
 
 
 if __name__ == "__main__":
